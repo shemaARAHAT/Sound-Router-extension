dee9fbcea0453a03673fca60fddbbeeaaf90d76a
alexyowl <AlexY228rus@gmail.com>
2025-06-02 04:00:01 +0500

    Source files

---
+++ SoundRouter_Firefox/background.js
@@ -0,0 +1,13 @@
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'injectScript') {
+    browser.scripting.executeScript({
+      target: { tabId: request.tabId },
+      func: setAudioChannel,
+      args: [request.channel]
+    });
+  }
+});
+
+function setAudioChannel(channel) {
+  window.dispatchEvent(new CustomEvent('audio_splitter_apply', { detail: channel }));
+}
